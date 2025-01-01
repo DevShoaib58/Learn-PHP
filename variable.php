5fc8176ced6707f35b75f8cc278eaf48dcdc86f2
DevShoaib58 <devshoaib58@gmail.com>
2025-01-01 19:46:01 +0500

    Learn to Use of variable in PHP

---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+    $a="Hello World!";
+    $b=4544;
+    echo $a;
+    echo $b;
+
+    $Name="Shoaib";
+    $age=28;
+    echo "<h1>"."My name is ".$Name . " and my age is " . $age ."</h1>";
+?>
