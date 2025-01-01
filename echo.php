35c0e2d5864ff906c121aff6be466eb167100455
DevShoaib58 <devshoaib58@gmail.com>
2025-01-01 19:39:24 +0500

    Adding HTML tag in PHP

--- echo.php
+++ echo.php
@@ -5,9 +5,11 @@
 echo "Hello echo Command !". "Hello echo 2 Command !";
 echo 5424;
 echo 54.24;
+
+echo "<h1>"."Hello echo Command !"."</h1>";
 ?>
 
 <?php 
 print "Hello print Command !";
-print "Hello print Command !","Hello print Command !"; 
+// print "Hello print Command !","Hello print Command !"; 
 ?>
