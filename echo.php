c9634221410be05c6ce8e9094fb2f66f8ba28b01
DevShoaib58 <devshoaib58@gmail.com>
2025-01-01 18:46:42 +0500

    Learn PHP Echo & Print Statement

---
+++ echo.php
@@ -0,0 +1,13 @@
+<?php 
+// echo "Hello echo Command !";
+// echo ("Hello echo Command !");
+// echo "Hello echo Command !", "Hello echo 2 Command !";
+echo "Hello echo Command !". "Hello echo 2 Command !";
+echo 5424;
+echo 54.24;
+?>
+
+<?php 
+print "Hello print Command !";
+print "Hello print Command !","Hello print Command !"; 
+?>
