dfd5db531e93f718f2099d70d11023d44b5e2cd5
DevShoaib58 <devshoaib58@gmail.com>
2025-01-24 15:10:37 +0500

    Comment in PHP

---
+++ comment.php
@@ -0,0 +1,11 @@
+<?php 
+$name = "Shoaib Qureshi";
+/*Multi Line of Comment*/
+/*$age = 21;
+$gender = "male";
+$country = "Pakistan";
+*/
+// $city = "Lahore";   Single Line of Comment
+
+echo "<h1>My name is $name. I am $age years old. I am a $gender. I live in $city, $country.<h1>";
+?>
