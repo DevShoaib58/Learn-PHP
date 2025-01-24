14836a0710e7633ec5f8ccaa354ae21b9ec43725
DevShoaib58 <devshoaib58@gmail.com>
2025-01-24 19:30:44 +0500

    Arithmetic Operator in PHP

---
+++ arthmeticOperator.php
@@ -0,0 +1,18 @@
+<?php 
+$a =20;
+$b=10;
+$c=$a+$b;
+$c=$a-$b;
+$c=$a*$b;
+$c=$a**$b;
+$c=$a/$b;
+$c=$a%$b;
+$c=$a++;
+$c=$a++;
+$c=$b--;
+$c=$b--;
+echo "Addition of $a and $b is $c <br>";
+echo "Subtraction  of $a and $b is $c <br>";
+echo "Multiply of $a and $b is $c <br>";
+echo "Power of $a and $b is $c <br>";
+?>
